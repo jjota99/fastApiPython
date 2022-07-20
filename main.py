8602e1cdde89a569e80d1fe2b31a3b53f4abf465
Jota <jguilherme1999@gmail.com>
2022-07-20 13:53:05 -0300

    add other endpoint

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from datetime import datetime
 
+from pydantic import BaseModel
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+# rota raiz
+@app.get("/")
+def read_root():
+    return {'hello': 'world'}
+
+# rota horario
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 
+@app.get("/horario")
+def horario():
+    return {current_time}
 
-@app.get("/")
-def read_root():
-    return 'Horario atual: %s' % current_time
